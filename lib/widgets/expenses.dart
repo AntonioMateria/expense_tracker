e84d069408a24fbfa0ec7cd6fc09e82b069f7810
Antonio <antoniomateria04@gmail.com>
2025-04-08 11:40:46 -0400

    Added Controller for title

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,22 +1,56 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widgets/expenses_list/new_expense.dart';
 import 'package:flutter/material.dart';
 
-class Expenses extends StatefulWidget{
+import '../models/expense.dart';
+
+class Expenses extends StatefulWidget {
   const Expenses({super.key});
-  
+
   @override
   State<Expenses> createState() {
     return _ExpensesState();
   }
 }
 
-class _ExpensesState extends State<Expenses>{
+class _ExpensesState extends State<Expenses> {
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => NewExpense(),
+    );
+  }
+
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Cheeseburger',
+        amount: 11.99,
+        date: DateTime.now(),
+        category: Category.food),
+    Expense(
+        title: 'Movie Ticket',
+        amount: 18.99,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Expense Tracker"),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
       body: Column(
         children: [
           Text("Chart"),
-          Text('Expenses List...'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
         ],
       ),
     );
