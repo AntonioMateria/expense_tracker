fe367743f9889c515b3304f08888f2cbd2fc166d
Antonio <antoniomateria04@gmail.com>
2025-04-17 12:17:44 -0400

    finished new expense form to save data

--- lib/widgets/expenses_list/new_expense.dart
+++ lib/widgets/expenses_list/new_expense.dart
@@ -5,8 +5,8 @@ import 'package:intl/intl.dart';
 final formatter = DateFormat.yMd();
 
 class NewExpense extends StatefulWidget{
-const NewExpense({super.key});
-
+const NewExpense({super.key, required this.onAddExpense});
+  final void Function(Expense expense) onAddExpense;
   @override
   State<NewExpense> createState() {
     return _NewExpenseState();
@@ -30,8 +30,38 @@ class _NewExpenseState extends State<NewExpense>{
         _selectedDate = pickedDate;
       });
   }
+    void _submitExpenseData(){
+    final enteredAmount = double.tryParse(_amountController.text);
+    final amountIsInvalid = enteredAmount==null || enteredAmount <= 0;
+    if (_titleController.text.trim().isEmpty || 
+    amountIsInvalid|| _selectedDate == null){
+      showDialog(context:context, builder: (ctx)=>
+        AlertDialog(
+          title: const Text('Invalid Input!'),
+          content: const Text('Please make sure valid title, amount, date were entered!'),
+          actions: [
+            TextButton(
+              onPressed: (){
+                Navigator.pop(ctx);
+              },
+              child: const Text('Okay'),
+            ),
+          ],
+        )
+      );
+      return;
+    }
+widget.onAddExpense(
+      Expense(
+        title: _titleController.text,
+        amount: enteredAmount,
+        date: _selectedDate!,
+        category: _selectedCategory,
+      ),
+    );
+    Navigator.pop(context);
+  }
 
-  
   @override
   void dispose() {
     _titleController.dispose();
@@ -42,7 +72,7 @@ class _NewExpenseState extends State<NewExpense>{
   @override
   Widget build(BuildContext context) {
     return  Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.fromLTRB(16, 48, 16, 16),
       child: Column(
         children: [
           TextField(
@@ -102,6 +132,7 @@ class _NewExpenseState extends State<NewExpense>{
                   });
                 }
               ),
+              Spacer(),
               ElevatedButton(
                 onPressed: (){
                   Navigator.pop(context);
@@ -110,13 +141,11 @@ class _NewExpenseState extends State<NewExpense>{
                 const Text('Cancel'),
               ),
               ElevatedButton(
-                onPressed: (){
-                  print(_titleController.text);
-                  print(_amountController.text);
-                },
-                child: 
-                const Text('Save Expense'),
+                onPressed: _submitExpenseData,
+                child: Text('Save Expense'),
               ),
+                const Text('Save Expense'),
+              
             ],
           )
         ],
